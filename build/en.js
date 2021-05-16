b23d56a06949fe7fa830752628a7f0130fbd4745
Austin Burk <apburk@sudomemo.net>
2021-05-16 21:29:07 +0000

    Add missing English translations

--- build/en.js
+++ build/en.js
@@ -1,11 +1,93 @@
 var messages = {
+    debug: {
+        NOT_READY_TO_LOAD: "Not ready to load (canLoad = false)",
+    },
     main: {
-        RECENT_USERS: 'Recent Users',
-        LATEST_POSTS: 'Latest Posts',
+        ADD_TO_FAVORITES: "Add to favorites",
+        ALBUM: 'Album',
+        CONTINUE_AS_USER: 'Continue as {name}',
+        ENTRIES: 'Entries',
         EVERYBODY_HAIKU: 'Everybody Haiku!',
+        FAVORITE_KEYWORDS: "Favorite Keywords",
+        HATENA_BLOG_TAG_ABOUT_WORD: "What is {word}?",
+        HATENA_BLOG_TAG: "Hatena Blog Tags",
         HOT_KEYWORDS: 'Hot Keywords',
-        CONTINUE_AS_USER: 'Continue as %s',
+        KEYWORDS: 'Keywords',
+        LATEST_POSTS: 'Latest Posts',
+        LOGIN_WITH_HATENA: 'Login with Hatena ID',
+        NEW_HATENA_BLOG_ENTRY_ABOUT_WORD: "Write a blog about {word}",
+        NO_BLOG_TAG_DATA_AVAILABLE: "No information available for this keyword",
+        READ_ABOUT_WORD_ON_HATENA_BLOG_TAG: "Read about {word}",
+        READ_MORE: "Read more…",
+        RECENT_USERS: 'Recent Users',
+        RELATED_KEYWORDS: "Related Keywords",
+        REMOVE_FROM_FAVORITES: "Remove from favorites",
+        SETTINGS: "Settings",
+        USERS_ENTRY: "{name}'s entry",
+        USERS_FEED: "{name}'s feed",
+        USERS_PROFILE_IMAGE: "{name}' profile image",
+        USERS: 'Users',
+        VIEW_ENTRIES_ABOUT_WORD: "View entries about {word}",
+    },
+    settings: {
+        EDIT_PROFILE_INFORMATION: "Edit your profile details",
+        JUMP_TO_SETTINGS_SECTION: "Jump to {section} section",
+        LAST_PROFILE_SYNC: "Last Profile Sync",
+        PLAY_STAR_SOUNDS: "Play Star sounds",
+        PREVIEW: "Preview",
+        PROFILE: "Profile",
+        SECTION_ACCOUNT: "Account",
+        SECTION_STARS: "Stars",
+        SETTINGS: "Settings",
+        SYNC_DATA_EXPLANATION: "Your profile name and description will match what's written on your Hatena Profile.",
+        SYNC_PROFILE_FROM_HATENA: "Sync now",
+    },
+    globalHeader: {
+        ANTENNA: 'Antenna',
+        HELP: 'Help',
+        MY_HATENA: 'My Hatena',
+        MY_PAGE: 'My Page',
+        PROFILE: 'Profile',
+        RECENT: 'Recent',
+        SEARCH: 'Search',
+        SERVICES_USED: 'Services used',
+        SETTINGS: 'Settings',
+    },
+    composer: {
+        ADD_EMOJI: "Add emoji",
+        ADD_IMAGE: "Add image",
+        ADD_LINK: "Add link",
+        ADD_MENTION: "Add mention",
+        BOLD: "Bold",
+        CANCEL: "Cancel",
+        DISCARD_THIS_MESSAGE: "Discard this message?",
+        ITALIC: "Italic",
+        JUMP_TO_EMOJI_GROUP: "Jump to {groupname}",
+        KEYWORD_PLACEHOLDER: "Keyword (optional)",
+        POST: "Post",
+        POSTING: "Posting",
+        SELECT_EMOJI_NAME: "Insert {emoji} emoji",
+        STRIKETHROUGH: "Strikethrough",
+        UNDERLINE: "Underline",
+    },
+    entry: {
+        DELETE_THIS_ENTRY_CONFIRM: "Do you want to delete this entry?",
+        REPLY: "Reply",
+        SHOW_REPLIES: "Show repies",
+    },
+    entryFeed: {
+        NEW_ENTRY_MODAL: "Show {count} new entry|Show {count} new entries"
+    },
+    notify: {
+        NOTIFICATIONS: "Notifications"
+    },
+    hatenaStar: {
+        ADD_STAR_BUTTON: "Add Star button",
+        ADD_STAR: "Add Star",
+        EXPAND_COLLAPSED_STARS: "Expand"
     }
+
 };
+
 
 export default messages;
